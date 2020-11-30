ffb08ce1ffad4e26745727f5b2adf1942ca150a2
Carlos Ibarra <ceibarrag@gmail.com>
2020-11-30 16:23:43 -0600

    Styles added to header

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from '../components/Header';
+import '../assets/styles/App.scss';
 
-const App = () => {
+const App = () => (
   <div className="App">
     <Header />
   </div>
-};
+);
 
 export default App;
